7abd78b969703e72b8df842b624812b5ee0617e6
Abman <abman95@hotmail.de>
2024-11-05 15:04:32 +0100

    feat: Add README.md for StickerSmash app
    - Included project description, features, prerequisites, installation instructions, and usage guidelines.
    - Added support and license sections.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,74 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs } from "expo-router";
+import  { Ionicons } from "@expo/vector-icons";
+import {getHeaderTitle} from "@react-navigation/elements";
+import {StatusBar} from "expo-status-bar";
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
-  );
+export default function TabsLayout() {
+    return (
+        <>
+        <StatusBar style="auto"></StatusBar>
+        <Tabs
+            screenOptions={{
+                ...tabStyleColors,
+            }}
+        >
+            <Tabs.Screen
+                name="index"
+                options={{
+                    headerTitle: "Sticker Smash",
+                    ...headerStyles,
+                    ...tabStylesHome,
+                }}
+            />
+            <Tabs.Screen
+                name="about"
+                options={{
+                    headerTitle: "About Sticker Smash",
+                    ...headerStyles,
+                    ...tabStylesAbout,
+                }}
+            />
+        </Tabs>
+        </>
+    );
 }
+
+
+const tabStyleColors = {
+    tabBarStyle: {
+        backgroundColor: 'black',
+    },
+    tabBarActiveTintColor: 'white',
+    tabBarInactiveTintColor: 'rgb(105, 105, 105)',
+};
+
+const tabStylesHome = {
+    tabBarIcon: ({ focused, color }) => (
+        <Ionicons
+            name={focused ? "home-sharp" : "home-outline"}
+            size={30}
+            color={"white"}
+        />
+    ),
+}
+
+const tabStylesAbout = {
+    tabBarIcon: ({ focused, color }) => (
+        <Ionicons
+            name={focused ? "information-circle" : "information-circle-outline"}
+            size={30}
+            color={"white"}
+        />
+    ),
+}
+
+const headerStyles = {
+    headerStyle: {
+        backgroundColor: 'black'
+    },
+    headerTitleStyle: {
+        fontSize: 18,
+        color: 'white'
+    },
+    headerTitleAlign: 'center'
+};
